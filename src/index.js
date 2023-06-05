dcdd73723f0e03f9cb13b0d95d3ba801ec088518
Igor <115535399+IgorVinnik85@users.noreply.github.com>
2023-06-05 13:06:14 +0300

    update02

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { App } from "components/App";
-import "./index.css";
-import { Provider } from "react-redux";
-import { persistor, store } from "redux/store";
-import { PersistGate } from "redux-persist/integration/react";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { App } from 'components/App';
+import './index.css';
+import { Provider } from 'react-redux';
+import { store } from 'redux/store';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
